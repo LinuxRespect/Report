a83bdc18e05cd13cab33305c6e163d5d91a97c73
o.rymarchuk <o.rymarchuk@eisgroup.com>
2023-04-25 12:37:54 +0300

    first commit

---
+++ src/test/java/org/example/Test1.java
@@ -0,0 +1,15 @@
+package org.example;/* Copyright © 2023 EIS Group and/or one of its affiliates. All rights reserved. Unpublished work under U.S. copyright laws.
+CONFIDENTIAL AND TRADE SECRET INFORMATION. No portion of this work may be copied, distributed, modified, or incorporated into any other media without EIS Group prior written consent.*/
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
